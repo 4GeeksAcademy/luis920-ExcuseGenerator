e4354d5274f9dadabe667e00e75df30e1eeceb78
Luis Galvan <67127384+luis920@users.noreply.github.com>
2024-10-05 03:20:12 +0000

    Generador de Excusas terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = [
+    "mi perro",
+    "el vecino",
+    "mi novia",
+    "mi hermano",
+    "mi amigo",
+    "mi papa"
+  ];
+  let accion = ["se comio", "mojo", "rompio", "robo", "escondio", "vendio"];
+  let que = [
+    "la tarea",
+    "el celular",
+    "mis tenis",
+    "mi ropa",
+    "las llaves",
+    "mi cartera"
+  ];
+  let cuando = [
+    "cuando me bañaba",
+    "cuando dormia",
+    "cuando comia",
+    "cuando no estaba",
+    "cuando estudiaba",
+    "cuando hacia ejercicio"
+  ];
+
+  document.querySelector(".excusa").innerHTML =
+    quien[Math.floor(Math.random() * quien.length)] +
+    " " +
+    accion[Math.floor(Math.random() * accion.length)] +
+    " " +
+    que[Math.floor(Math.random() * que.length)] +
+    " " +
+    cuando[Math.floor(Math.random() * cuando.length)];
+
+  document.getElementById("#boton");
+  addEventListener("click", _ => {
+    window.location.reload(true);
+  });
 };
